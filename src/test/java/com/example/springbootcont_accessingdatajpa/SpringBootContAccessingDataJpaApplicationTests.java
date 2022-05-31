5ed9e8522c1b81213ea5478b517c26366b462f3f
China-a <China.allford@gmail.com>
2022-05-31 14:41:09 -0400

    SpringBootCont_AccessingDataJPA

---
+++ src/test/java/com/example/springbootcont_accessingdatajpa/SpringBootContAccessingDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcont_accessingdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootContAccessingDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
